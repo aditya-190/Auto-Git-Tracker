9be5e584317630042385d38b7a503c17d67901ee
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-14 16:10:40 +0530

    Changed: script.py

--- script.py
+++ script.py
@@ -1,10 +1,39 @@
-import subprocess
+import subprocess, re
+from datetime import datetime
 
 def run(*args):
     return subprocess.check_call(['git'] + list(args))
 
 
-run("status")
+commitMessage = ""
+
+status = subprocess.getoutput('git status')
+if checkIfNecessary := re.search('nothing to commit, working tree clean', status):
+    print("No Commit Required.")
+    exit()
+
+else:
+    searchModified = re.findall('modified:(.*)\n', status)
+    for modified in searchModified:
+        commitMessage += "Changed: " + modified.strip() + "\n"
+
+    searchAdded = re.findall('added:(.*)\n', status)
+    for added in searchAdded:
+        commitMessage += "Added: " + added.strip() + "\n"
+
+
+    searchDeleted = re.findall('deleted:(.*)\n', status)
+    for deleted in searchDeleted:
+        commitMessage += "Deleted: " + deleted.strip() + "\n"
+
+
+    searchRenamed = re.findall('renamed:(.*)\n', status)
+    for renamed in searchRenamed:
+        commitMessage += "Renamed: " + renamed.strip() + "\n"
+
+print(commitMessage)
+
 run("add", ".")
-run("commit", "-m", "Testing Purpose - 3")
+run("commit", "-m", commitMessage)
 run("push")
+
