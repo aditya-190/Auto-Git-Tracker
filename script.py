629f9f41d8569c16f8e6d1eb583115f9bc01792e
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-14 16:44:14 +0530

    Detailed Commit (Auto Tracker) - Modified: script.py - From Auto Tracker.

--- script.py
+++ script.py
@@ -1,43 +1,42 @@
 import subprocess, re
 from datetime import datetime
 
-def run(*args):
-    return subprocess.check_call(['git'] + list(args))
+def run():
+    subprocess.check_call(['git'] + ['add'] + ['.'])
+    commitMessage = "Detailed Commit (Auto Tracker) - \n"
+    status = subprocess.getoutput('git status')
 
-
-run("add", ".")
-commitMessage = "Detailed Commit (Auto Tracker) - \n"
-status = subprocess.getoutput('git status')
-
-if re.search('Your branch is ahead of', status):
-    print("Already Commited - Just a push away :)")
-    run("push")
-    exit()
+    if re.search('Your branch is ahead of', status):
+        print("Already Commited - Just a push away :)")
+        run("push")
+        return
     
-elif checkIfNecessary := re.search('nothing to commit, working tree clean', status):
-    print("Nothing to commit :(")
-    exit()
+    elif checkIfNecessary := re.search('nothing to commit, working tree clean', status):
+        print("Nothing to commit :(")
+        return
 
-else:
-    searchModified = re.findall('modified:(.*)\n', status)
-    for modified in searchModified:
-        commitMessage += "Modified: " + modified.strip() + " - From Auto Tracker.\n"
+    else:
+        searchModified = re.findall('modified:(.*)\n', status)
+        for modified in searchModified:
+            commitMessage += "Modified: " + modified.strip() + " - From Auto Tracker.\n"
 
-    searchAdded = re.findall('new file:(.*)\n', status)
-    for added in searchAdded:
-        commitMessage += "Added: " + added.strip() + " - From Auto Tracker.\n"
-
-
-    searchDeleted = re.findall('deleted:(.*)\n', status)
-    for deleted in searchDeleted:
-        commitMessage += "Removed: " + deleted.strip() + " - From Auto Tracker.\n"
+        searchAdded = re.findall('new file:(.*)\n', status)
+        for added in searchAdded:
+            commitMessage += "Added: " + added.strip() + " - From Auto Tracker.\n"
 
 
-    searchRenamed = re.findall('renamed:(.*)\n', status)
-    for renamed in searchRenamed:
-        commitMessage += "Renamed: " + renamed.strip() + " - From Auto Tracker.\n"
+        searchDeleted = re.findall('deleted:(.*)\n', status)
+        for deleted in searchDeleted:
+            commitMessage += "Removed: " + deleted.strip() + " - From Auto Tracker.\n"
 
-print(commitMessage)
-run("commit", "-m", commitMessage)
-run("push")
-exit()
+
+        searchRenamed = re.findall('renamed:(.*)\n', status)
+        for renamed in searchRenamed:
+            commitMessage += "Renamed: " + renamed.strip() + " - From Auto Tracker.\n"
+
+    print(commitMessage)
+    subprocess.check_call(['git'] + ['commit'] + ['-m'] + [commitMessage])
+    subprocess.check_call(['git'] + ['push'])
+    return
+
+run()
